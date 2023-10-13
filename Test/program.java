1c08ad914ee60c98a8109b9ab3c3e07a1425aa94
you <you@example.om>
2023-10-13 13:57:53 +0200

    Test

---
+++ Test/program.java
@@ -0,0 +1,23 @@
+// public class program {
+//     public static void main(String[] args) {
+//         System.out.println(("Yes"));
+//     }
+
+    
+// }
+
+package Test;
+
+/**
+ * program
+ */
+public class program {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 2;
+        System.out.println(a*b);
+    }
+    
+
+    
+}
